e98b33212adda2a805bfae4850d5c23827a38e05
Graham Campbell <graham@alt-three.com>
2015-11-08 19:49:39 +0000

    Improved environment specific code

--- src/SentryServiceProvider.php
+++ src/SentryServiceProvider.php
@@ -12,7 +12,9 @@
 namespace AltThree\Sentry;
 
 use Illuminate\Contracts\Foundation\Application;
+use Illuminate\Foundation\Application as LaravelApplication;
 use Illuminate\Support\ServiceProvider;
+use Laravel\Lumen\Application as LumenApplication;
 use Raven_Client as Sentry;
 
 /**
@@ -44,9 +46,9 @@ class SentryServiceProvider extends ServiceProvider
     {
         $source = realpath(__DIR__.'/../config/sentry.php');
 
-        if (class_exists('Illuminate\Foundation\Application', false) && $app->runningInConsole()) {
+        if ($app instanceof LaravelApplication && $app->runningInConsole()) {
             $this->publishes([$source => config_path('sentry.php')]);
-        } elseif (class_exists('Laravel\Lumen\Application', false)) {
+        } elseif ($app instanceof LumenApplication) {
             $app->configure('sentry');
         }
 
